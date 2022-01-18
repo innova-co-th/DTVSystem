aae5c630c94ea7631734a6a4f15f84ed927c0362
achadsiri <achadsiri@gmail.com>
2022-01-18 09:13:55 +0700

    20220118:0913 => Merge

---
+++ Controllers/ProductionResultAllController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace DTVSystem.Controllers
+{
+    public class ProductionResultAllController : Controller
+    {
+        public IActionResult ProductionResultAll()
+        {
+
+            //Creating the List of SelectListItem, this list you can bind from the database.
+            List<SelectListItem> ProductionResultMode = new()
+            {
+                new SelectListItem { Value = "1", Text = "Real-Time Monitoring" },
+                new SelectListItem { Value = "2", Text = "History" },
+             
+            };
+
+            List<SelectListItem> Shift = new()
+            {
+                new SelectListItem { Value = "1", Text = "All" },
+                new SelectListItem { Value = "2", Text = "A" },
+                new SelectListItem { Value = "3", Text = "B" },
+
+            };
+
+            //assigning SelectListItem to view Bag
+            ViewBag.ProductionResultMode = ProductionResultMode;
+            ViewBag.shift = Shift;
+
+            return View();
+        }
+    }
+}
