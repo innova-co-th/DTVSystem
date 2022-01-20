8763c4b43d7d59f58548b09d341a42ee04c45ce8
kwang <kwang@innova.co.th>
2022-01-20 10:53:18 +0700

    2022/01/20_1052 : Create Dynamic Table Production result All

---
+++ ViewModels/PRA0010ViewModels.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace DTVSystem.ViewModels
+{
+    public class PRA0010ViewModels
+    {
+        public List<SelectListItem> GridHeader { get; set; }
+        public List<GridBody>  GBody { get; set; }
+
+
+    }
+
+    public class GridBody
+    {
+        public List<string> line { get; set; }
+        public List<string> Plan { get; set; }
+        public List<string> Actual { get; set; }
+        public List<string> Diff { get; set; }
+    }
+}
