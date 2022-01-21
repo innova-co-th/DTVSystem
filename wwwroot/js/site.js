077aa8751fd8a74de06c88c73bc50495a5dcdf8d
anatchadsiri <nat@innova.co.th>
2022-01-21 09:04:34 +0700

    20220121:0903 ==> Add modal in _Layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+ShowError = (title, message) => {
+    $("#form-modal-Error .modal-title").html(title);
+    $("#form-modal-Error .modal-body").html(message);
+    $("#form-modal-Error").modal('show');
+}
+
+ShowInfomation = (title, message) => {
+    $("#form-modal-Infomation .modal-title").html(title);
+    $("#form-modal-Infomation .modal-body").html(message);
+    $("#form-modal-Infomation").modal('show');
+}
+
+ShowWarining = (title, message) => {
+    $("#form-modal-Warining .modal-title").html(title);
+    $("#form-modal-Warining .modal-body").html(message);
+    $("#form-modal-Warining").modal('show');
+}
+
+ShowPopup = (title) => {
+    $("#form-modal .modal-body").html("");
+    $("#form-modal .modal-title").html(title);
+    $("#form-modal").modal('show');
+}
+
+CallAjaxClick = (url) => {
+    $.ajax({
+        type: "POST",
+        url: url,
+        success: function (res) {
+            alert(res);
+        }
+    })
+}
